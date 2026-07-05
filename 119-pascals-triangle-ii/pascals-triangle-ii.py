7df27c79315c771513a959ece4caf30d032c16ad
Samarth Suresh Ghare <gharesamarth2@gmail.com>
2026-07-05 13:22:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (88.21%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -2,7 +2,11 @@ class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         row = [1]
 
-        for  _ in range(rowIndex):
-            row = [left + right for left, right in zip([0]+row, row + [0])]
+        for  i in range(rowIndex):
+            newRow = [0] * (len(row)+1)
+            for j in range(len(row)):
+                newRow[j] += row[j]
+                newRow[j+1]+= row[j]
 
+            row = newRow
         return row
