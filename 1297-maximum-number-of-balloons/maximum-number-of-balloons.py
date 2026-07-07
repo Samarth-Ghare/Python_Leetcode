4af5f4f8a30f3bdba22aafab580da33b4558d54a
Samarth Suresh Ghare <gharesamarth2@gmail.com>
2026-07-07 12:25:26 +0530

    Time: 3 ms (56.96%) | Memory: 19.4 MB (37.01%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,4 +1,11 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        f = Counter(text)
-        return min(f["b"], f["a"], f["l"] >> 1, f["o"] >> 1, f["n"])
+        target="balloon"
+        counter=Counter(text)
+        ballonCounter=Counter(target)
+        res=float("inf")
+        for char in target:
+            # print("counter[char]//ballonCounter[char]",counter[char]//ballonCounter[char])
+            res=min(res,counter[char]//ballonCounter[char])
+        return 0 if res==float("inf") else res
+        
