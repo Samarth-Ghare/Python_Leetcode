9bbab371e065438694ee92cc1cca67759cfe2b61
Samarth Suresh Ghare <gharesamarth2@gmail.com>
2026-07-08 18:30:22 +0530

    Time: 379 ms (96.30%) | Memory: 57.6 MB (24.69%) - LeetSync

--- 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.py
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.py
@@ -1,41 +1,31 @@
+MOD = 10 ** 9 + 7
+pow10 = [1] * 100001
+for i in range(1, 100001):
+    pow10[i] = pow10[i - 1] * 10 % MOD
+
+
 class Solution:
     def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
         n = len(s)
-        m = len(queries)
-        MOD = 10**9 + 7
+        sum = [0] * (n + 1)
+        x = [0] * (n + 1)
+        count = [0] * (n + 1)
+        for i, c in enumerate(s):
+            d = int(c)
+            sum[i + 1] = sum[i] + d
+            x[i + 1] = (x[i] * 10 + d) % MOD if d > 0 else x[i]
+            count[i + 1] = count[i] + (d > 0)
 
-        prefSum = [0] * (n + 1)
-        prefVal = [0] * (n + 1)
-        prefCnt = [0] * (n + 1)
-        power = [1] * (n + 1)
+        ans = [0] * len(queries)
+        for i, (l, r) in enumerate(queries):
+            length = count[r + 1] - count[l]
+            ans[i] = (x[r + 1] - x[l] * pow10[length]) * (sum[r + 1] - sum[l]) % MOD
 
-        for i in range(1, n + 1):
-            power[i] = (power[i - 1] * 10) % MOD
-
-        for i in range(n):
-            d = int(s[i])
-
-            prefSum[i + 1] = prefSum[i] + d
-            prefCnt[i + 1] = prefCnt[i] + (1 if d != 0 else 0)
-
-            if d == 0:
-                prefVal[i + 1] = prefVal[i]
-            else:
-                prefVal[i + 1] = (prefVal[i] * 10 + d) % MOD
-
-        result = [0] * m
-
-        for i in range(m):
-            l, r = queries[i]
-
-            length = prefCnt[r + 1] - prefCnt[l]
-
-            start = prefVal[l]
-            end = prefVal[r + 1]
-
-            x = (end - (start * power[length]) % MOD + MOD) % MOD
-            sm = prefSum[r + 1] - prefSum[l]
-
-            result[i] = (x * sm) % MOD
-
-        return result
+        return ans
+        # preprocessing:
+        # time: O(maximum possible string length)
+        # space: O(maximum possible string length)
+        # n = the length of the input string
+        # m = the number of queries
+        # time: O(n + m)
+        # space: O(n)
