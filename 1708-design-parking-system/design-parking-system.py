012dc50355a989633a92b3525444375af927d90c
Samarth Suresh Ghare <gharesamarth2@gmail.com>
2026-07-09 14:50:44 +0530

    Time: 12 ms (68.37%) | Memory: 19.8 MB (19.22%) - LeetSync

--- 1708-design-parking-system/design-parking-system.py
+++ 1708-design-parking-system/design-parking-system.py
@@ -1,19 +1,13 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        # Number of empty slots for each type of car
-        self.empty = [big, medium, small]
+        self.availableSpots = [0,big,medium,small]
 
     def addCar(self, carType: int) -> bool:
-        # If space is available, allocate and return True
-        if self.empty[carType - 1] > 0:
-            self.empty[carType - 1] -= 1
-            return True
+        if self.availableSpots[carType] > 0: 
+            self.availableSpots[carType] -=1
+            return True 
+        else: 
+            return False
+    
 
-        # Else, return False
-        return False
-
-
-# Your ParkingSystem object will be instantiated and called as such:
-# obj = ParkingSystem(big, medium, small)
-# param_1 = obj.addCar(carType)
