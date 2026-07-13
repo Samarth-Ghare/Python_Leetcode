f78d2d3712da70bc2b939879ce4c56cc3639126c
Samarth Suresh Ghare <gharesamarth2@gmail.com>
2026-07-13 12:17:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (64.31%) - LeetSync

--- 1212-sequential-digits/sequential-digits.py
+++ 1212-sequential-digits/sequential-digits.py
@@ -1,9 +1,11 @@
 class Solution:
-    q  = [*range(1,10)]
-    for x in q:
-        d = x %10
-        if d < 9:
-            q.append(x * 10 + d + 1)
-
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        return [x for x in self.q if low <= x <= high]
+        n = '123456789'
+        l = []
+        for i in range(0, len(n)):
+            for j in range(i + 1, len(n) + 1):
+                s=int(n[i:j])
+                if s>= low and s<=high:
+                    l.append(s)
+        l.sort()
+        return l
