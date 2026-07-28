c1b619a40c8d9a60241e31311ebdc6fb037b94b2
Samarth Suresh Ghare <gharesamarth2@gmail.com>
2026-07-28 20:27:31 +0530

    Time: 3 ms (90.13%) | Memory: 13.5 MB (49.32%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,14 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        temp = []
-        for i in range(0,n):
-            if nums[i] != 0:
-                temp.append(nums[i])
-        nz = len(temp)
-        for i in range(0, nz):
-            nums[i] = temp[i]
-        for i in range(nz,n):
-            nums[i] = 0
-            
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] !=0 :
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
