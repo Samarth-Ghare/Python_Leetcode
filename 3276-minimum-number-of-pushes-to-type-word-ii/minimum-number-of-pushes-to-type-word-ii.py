cf4baefd9766281e6324bfd503a31a46a1861e0e
Samarth Suresh Ghare <gharesamarth2@gmail.com>
2026-07-31 22:51:18 +0530

    Time: 15 ms (100.00%) | Memory: 20 MB (77.26%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -1,15 +1,9 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        char_count = [0] * 26
+        counts = [word.count(chr(ord('a') + i)) for i in range(26)] 
+        arr = sorted(counts, reverse=True)
+        total = 0
+        for group, i in enumerate(range(0, len(arr), 8), start=1):
+            total += sum(arr[i:i+8]) * group
 
-        for ch in word:
-            char_count[ord(ch) - ord('a')] += 1
-
-        char_count.sort(reverse = True)
-
-        min_push_count = 0
-
-        for i in range(26):
-            min_push_count += char_count[i] * (i // 8 + 1)
-
-        return min_push_count
+        return total
