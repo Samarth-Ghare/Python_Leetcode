b6197fe25ad00b65ec91c25e0d77f8f75bd7dd97
Samarth Suresh Ghare <gharesamarth2@gmail.com>
2026-08-05 22:02:13 +0530

    Time: 200 ms (97.73%) | Memory: 99.5 MB (96.97%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.py
+++ 3561-remove-methods-from-project/remove-methods-from-project.py
@@ -1,32 +1,21 @@
 class Solution:
     def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
-        edges = [[] for _ in range(n)]
-        in_degrees = [0] * n
+        adj = [[] for _ in range(n)]
+        for u, v in invocations:
+            adj[u].append(v)
+        
+        suspicious = [False]*n
+        suspicious[k] = True
+
+        q = deque([k])
+        while q:
+            u = q.popleft()
+            for v in adj[u]:
+                if not suspicious[v]:
+                    suspicious[v] = True
+                    q.append(v)
 
         for u, v in invocations:
-            edges[u].append(v)
-            in_degrees[v] += 1
-        
-        queue = collections.deque([k])
-        sus = bytearray(n)
-        sus[k] = 1
-
-        while queue:
-            u = queue.popleft()
-            for v in edges[u]:
-                in_degrees[v] -= 1
-
-                if sus[v] == 0:
-                    queue.append(v)
-                    sus[v] = 1
-
-        can_remove_all = True
-        for i in range(n):
-            if sus[i] == 1 and in_degrees[i] > 0:
-                can_remove_all = False
-                break
-
-        if not can_remove_all:
-            return list(range(n))
-
-        return [i for i in range(n) if sus[i] == 0]
+            if not suspicious[u] and suspicious[v]:
+                return list(range(n))
+        return [i for i in range(n) if not suspicious[i]]  
