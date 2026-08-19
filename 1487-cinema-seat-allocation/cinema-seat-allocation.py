a089449bc648333688abf4ce07f8ee5ea6125e5a
Samarth Suresh Ghare <gharesamarth2@gmail.com>
2026-08-19 19:52:35 +0530

    Time: 27 ms (87.92%) | Memory: 22.6 MB (89.46%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.py
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.py
@@ -1,22 +1,11 @@
+MAP_COUNT = [2] + [int(not (idx & 240) or not (idx & 60) or not (idx & 15))
+                    for idx in range(1, 256)]
+MAP_POWER = [0, 0, 1, 2, 4, 8, 16, 32, 64, 128, 0]
+
 class Solution:
-    def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
-        seats = collections.defaultdict(set)
-        for i, j in reservedSeats:
-            if j in{4,5}:
-                seats[i].add(0)
-                seats[i].add(1)
-            elif j in{6,7}:
-                seats[i].add(1)
-                seats[i].add(2)
-            elif j in{8,9}:
-                seats[i].add(2)
-            elif j in{2,3}:
-                seats[i].add(0)
-
-            res = 2*n
-        
-        for i in seats:
-            if len(seats[i]) == 3: res -= 2
-            else: res -= 1
-
-        return res
+    def maxNumberOfFamilies(self, n: int, reservedSeats) -> int:
+        map_count, map_power = MAP_COUNT, MAP_POWER
+        rows  = collections.defaultdict(int)
+        for row, seat in reservedSeats:
+            rows[row] |= map_power[seat]
+        return sum(map_count[row] for row in rows.values()) + 2*(n - len(rows))
