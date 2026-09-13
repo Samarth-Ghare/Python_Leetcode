5c75ba9c1ea45af794265b460f90a59d8c3fd01f
Samarth Suresh Ghare <gharesamarth2@gmail.com>
2026-09-14 00:03:23 +0530

    Time: 1 ms (100.00%) | Memory: 32.8 MB (6.35%) - LeetSync

--- 864-image-overlap/image-overlap.py
+++ 864-image-overlap/image-overlap.py
@@ -1,14 +1,15 @@
+import numpy as np
+
 class Solution:
     def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+        a = np.array(img1, dtype=np.int8)
+        b = np.array(img2, dtype=np.int8)
+        b = np.flip(b)
+
         n = len(img1)
-        A = [(i, j) for i in range(n) for j in range(n) if img1[i][j] == 1]
-        B = [(i, j) for i in range(n) for j in range(n) if img2[i][j] == 1]
-        cnt = [[0] * (2 * n) for _ in range(2 * n)]
-        best = 0
-        for ax, ay in A:
-            for bx, by in B:
-                dx = bx - ax + n
-                dy = by - ay + n
-                cnt[dx][dy] += 1
-                best = max(best, cnt[dx][dy])
-        return best
+        shape = (n * 2 - 1, n * 2 - 1)
+        fa = np.fft.fft2(a, shape)
+        fb = np.fft.fft2(b, shape)
+        c = np.rint(np.fft.ifft2(fa * fb).real)
+
+        return int(c.max())
