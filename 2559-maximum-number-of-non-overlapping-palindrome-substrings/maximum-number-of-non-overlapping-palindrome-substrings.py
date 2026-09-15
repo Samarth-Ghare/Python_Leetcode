9e2b2bd385841a3303f5f43498d6e3cdcb47f563
Samarth Suresh Ghare <gharesamarth2@gmail.com>
2026-09-15 12:47:53 +0530

    Time: 3 ms (95.88%) | Memory: 19.4 MB (72.68%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.py
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -1,20 +1,29 @@
 class Solution:
     def maxPalindromes(self, s: str, k: int) -> int:
+        
+        def check(l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+
         n = len(s)
         ans = 0
-        end = -1
+        start = 0
 
-        for i in range(n):
-            for l0 in(i - 1, i):
-                l, r = l0, i
+        for r in range(k - 1, n):
+            l = r - k + 1
 
-                while l >= 0 and r < n and s[l] == s[r]:
-                    if r - l + 1 >= k and l > end:
-                        ans += 1
-                        end = r
-                        break
-
-                    l -= 1
-                    r += 1
+            if l >= start and check(l, r):
+                ans += 1
+                start = r + 1
+                continue
+            
+            l = r - k
+            if l >= start and check(l, r):
+                ans += 1
+                start = r + 1
 
         return ans
