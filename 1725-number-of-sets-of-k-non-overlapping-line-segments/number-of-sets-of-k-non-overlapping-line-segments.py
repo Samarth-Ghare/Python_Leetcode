512a50819b656cc0d3b47f2ec61a96911872601d
Samarth Suresh Ghare <gharesamarth2@gmail.com>
2026-09-16 19:18:55 +0530

    Time: 2 ms (67.24%) | Memory: 19.5 MB (55.17%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.py
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.py
@@ -1,16 +1,3 @@
 class Solution:
     def numberOfSets(self, n: int, k: int) -> int:
-        mod = 10 ** 9 + 7
-        dp = [[0] * (k+1) for _ in range(n)]
-
-        for i in range(n):
-            dp[i][0] = 1
-
-        for j in range(1, k+1):
-            total = 0
-            for i in range(1, n):
-                total = (total + dp[i-1][j-1])%mod
-                dp[i][j] = (dp[i-1][j] + total)%mod
-        return dp[n-1][k]
-
-        
+        return math.comb(n+k-1,2*k)%(10**9+7)
